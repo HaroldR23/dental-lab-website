ed506e26ea6980bb07d4be8853fd1d93619a2c66
Harold Rodriguez <107003395+HaroldR23@users.noreply.github.com>
2024-03-08 19:31:04 -0300

    Hr/dlw implement nav bar organism component (#5)
    * feat: implement nav bar component
    
    * feat: add styles for Anchor, AnchorItems and NavBar components

---
+++ src/components/organisms/NavBar/NavBar.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import NavBar from "./NavBar";
+
+describe("NavBar", () => {
+    const logoUrl = "https://example.com/logo.png";
+    const anchorItems = [
+        { title: "Home", href: "/", id: "home" },
+        { title: "About", href: "/about", id: "about"},
+        { title: "Contact", href: "/contact", id: "contact"},
+    ];
+    beforeEach(() => {
+        render(<NavBar logoUrl={logoUrl} anchorItems={anchorItems} />);
+    });
+    it("renders the logo", () => {        
+        const logoElement = screen.queryByRole("img");
+        const srcAttribute = logoElement?.getAttribute("src");
+        const altAttribute = logoElement?.getAttribute("alt");
+
+        expect(srcAttribute).toBe(logoUrl);
+        expect(altAttribute).toBe(logoUrl);
+    });
+
+    it("renders the anchor items", () => {
+        const anchorElements = screen.queryByRole("list");
+        expect(anchorElements?.childElementCount).toBe(anchorItems.length);
+    });
+});
