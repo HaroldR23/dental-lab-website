3e6d861ad7fd7dac00674d4d9731ecd1f12f0471
HaroldR23 <harold.rodriguez@ioet.com>
2024-03-08 22:32:13 -0300

    feat: implement Paragraph component

---
+++ src/components/atoms/Paragraph/Paragraph.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import Paragraph from "./Paragraph";
+
+describe("Paragraph", () => {
+    it("renders the paragraph component with the provided text content", () => {
+        const textContent = "This is a test paragraph.";
+        const { container } = render(<Paragraph textContent={textContent} />);
+
+        const paragraphElement = container.querySelector(".paragraph");
+        
+        expect(paragraphElement?.firstChild?.textContent).toBe(textContent);
+    });
+});
---
+++ src/components/atoms/Paragraph/Paragraph.tsx
@@ -0,0 +1,9 @@
+import { ParagraphPropTypes } from "./ParagraphPropTypes";
+
+const Paragraph = ({ textContent }: ParagraphPropTypes) => {
+    return (
+        <p role="paragraph" className="paragraph">{textContent}</p>
+    )
+};
+
+export default Paragraph;
