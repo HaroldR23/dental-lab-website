8b91d85f8ce293909cb4e0c87dd8cfa030eb0246
Harold Rodriguez <107003395+HaroldR23@users.noreply.github.com>
2024-03-09 09:50:13 -0300

    feat: implement InformationContento component (#10)

---
+++ src/components/molecules/InformationContent/InformationContent.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import InformationContent from "./InformationContent";
+import { ReactElement } from "react";
+
+describe("InformationContent", () => {
+    it("should render the title and children correctly", () => {
+        const title = "Test Title";
+        const children = [<div key={0}>Test Children1</div>, <div key={1}>Test Children2</div>];
+
+        render(<InformationContent title={title} children={children}/>);
+
+        const titleElement = screen.getByRole("heading");
+        const childrenElements = screen.queryAllByRole("listitem");
+
+        expect(titleElement.textContent).toBe(title);
+        expect(childrenElements.length).toBe(children.length);
+        childrenElements.forEach((element, index) => {
+            expect(element.textContent).toBe(children[index].props.children);
+        });
+    });
+
+    it("should not render any list item", () => {
+        const title = "Test Title";
+        const children: ReactElement[] = [];
+
+        render(<InformationContent title={title} children={children}/>);
+        const childrenElements = screen.queryAllByRole("listitem");
+
+        expect(childrenElements).toHaveLength(0);
+    });
+});
