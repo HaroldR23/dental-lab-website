b282e04ae313c43850c70ade837b9750cd14d5e0
HaroldR23 <harold.rodriguez@ioet.com>
2024-03-17 17:26:50 -0300

    refactor: add isntructions to run the app

--- src/components/molecules/ButtonLink/ButtonLink.tsx
+++ src/components/molecules/ButtonLink/ButtonLink.tsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { ButtonLinkPropTypes } from "./ButtonLinkPropTypes";
 
 const ButtonLink = ({ text, icon, disabled }: ButtonLinkPropTypes) => {
     return (
-        <Link to="/products">
+        <a href="/products">
             <button className="buttonLink" disabled={disabled}>
                 {icon}
                 <div>{text}</div>
             </button>
-        </Link>
+         </a>
     )
 }
 
