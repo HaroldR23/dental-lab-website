87e5f1c053f32b948df91d2a610b5d1ab94497a4
HaroldR23 <harold.rodriguez@ioet.com>
2024-07-29 22:43:05 -0300

    implement CreateAppointmentForm component

---
+++ src/components/molecules/CreateAppointmentForm/CreateAppointmentFormPropTypes.ts
@@ -0,0 +1,8 @@
+import { MouseEventHandler } from "react";
+
+export interface CreateAppointmentFormPropTypes {
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handleClick:  MouseEventHandler<HTMLButtonElement>;
+    disabledSelect: boolean;
+    errors: {[key: string]: string};
+}
---
+++ src/constants/appointmentConstants.ts
@@ -0,0 +1,18 @@
+export const SELECT_TIME_OPTIONS = [
+    { value: "08:00", label: "08:00 - 09:00" },
+    { value: "09:00", label: "09:00 - 10:00" },
+    { value: "10:00", label: "10:00 - 11:00" },
+    { value: "11:00", label: "11:00 - 12:00" },
+    { value: "12:00", label: "12:00 - 13:00" },
+    { value: "13:00", label: "13:00 - 14:00" },
+    { value: "14:00", label: "14:00 - 15:00" },
+    { value: "15:00", label: "15:00 - 16:00" },
+    { value: "16:00", label: "16:00 - 17:00" },
+    { value: "17:00", label: "17:00 - 18:00" },
+];
+
+export const NAME_INPUT_ERROR_MESSAGE = "Ingrese el nombre del paciente";
+export const EMAIL_INPUT_ERROR_MESSAGE = "Ingrese el correo del paciente";
+export const EMAIL_VALIDATION_ERROR_MESSAGE = "El correo no es válido";
+export const TIME_INPUT_ERROR_MESSAGE = "Seleccione la hora de la cita";
+export const PHOTO_FORM = "https://i.imgur.com/GZgIXF1.png";
