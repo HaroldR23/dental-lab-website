9272237c580609dd6e13ac91bc8b0df78ee0b28a
HaroldR23 <harold.rodriguez@ioet.com>
2024-08-28 19:17:35 -0300

    refactor: create a wrapper for http calls

--- src/services/appointmentServices.ts
+++ src/services/appointmentServices.ts
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { API_URL } from "../constants/productConstants";
 import { AppointmentModel } from "../models/AppointmentModel";
+import { RequestBody, getFetchMethod, postFetchMethod } from "./utils/httpClient";
 
 export const getAllAppointmentsService = async () => {
     try {
-        const response = await fetch(`${API_URL}appointments`, {method: "GET"});
-        const data = await response.json();
-
+        const data = await getFetchMethod(`${API_URL}appointments`);
         const dataResponse: AppointmentModel[] = data.map((appointment: any) => (
             {
                 id: appointment.id,
@@ -15,31 +14,29 @@ export const getAllAppointmentsService = async () => {
                 patientName: appointment.patient_name,
                 patientEmail: appointment.patient_email
             }
-    ));
-    return dataResponse;
+        ));
+
+        return dataResponse;
 
     } catch (error: any) {
         throw new Error(error);
     }
-}
+};
 
 
 export const createAppointmentService = async (appointment: AppointmentModel) => {
     try {
-        await fetch(`${API_URL}appointments`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                date: appointment.date,
-                time: appointment.time,
-                patient_name: appointment.patientName,
-                patient_email: appointment.patientEmail,
-                patient_phone: appointment.patientPhone
-            }),
-        });
+        const body: RequestBody = {
+            date: appointment.date,
+            time: appointment.time,
+            patient_name: appointment.patientName,
+            patient_email: appointment.patientEmail,
+            patient_phone: appointment.patientPhone
+        };
+
+        await postFetchMethod(`${API_URL}appointments`, body);
+
     } catch (error: any) {
         throw new Error(error);
     }
-}
+};
