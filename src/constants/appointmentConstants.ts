ba8e73a1fd587536cadfe08a6ceef19dd47b559f
Harold Rodriguez <107003395+HaroldR23@users.noreply.github.com>
2024-09-01 14:13:50 -0300

    refactor: follow 2 spaces indentantion pattern in constants folder (#37)

--- src/constants/appointmentConstants.ts
+++ src/constants/appointmentConstants.ts
@@ -1,14 +1,14 @@
 export const SELECT_TIME_OPTIONS = [
-    { value: "08:00", label: "08:00 - 09:00" },
-    { value: "09:00", label: "09:00 - 10:00" },
-    { value: "10:00", label: "10:00 - 11:00" },
-    { value: "11:00", label: "11:00 - 12:00" },
-    { value: "12:00", label: "12:00 - 13:00" },
-    { value: "13:00", label: "13:00 - 14:00" },
-    { value: "14:00", label: "14:00 - 15:00" },
-    { value: "15:00", label: "15:00 - 16:00" },
-    { value: "16:00", label: "16:00 - 17:00" },
-    { value: "17:00", label: "17:00 - 18:00" },
+  { value: "08:00", label: "08:00 - 09:00" },
+  { value: "09:00", label: "09:00 - 10:00" },
+  { value: "10:00", label: "10:00 - 11:00" },
+  { value: "11:00", label: "11:00 - 12:00" },
+  { value: "12:00", label: "12:00 - 13:00" },
+  { value: "13:00", label: "13:00 - 14:00" },
+  { value: "14:00", label: "14:00 - 15:00" },
+  { value: "15:00", label: "15:00 - 16:00" },
+  { value: "16:00", label: "16:00 - 17:00" },
+  { value: "17:00", label: "17:00 - 18:00" },
 ];
 
 export const NAME_INPUT_ERROR_MESSAGE = "Ingrese el nombre del paciente";
