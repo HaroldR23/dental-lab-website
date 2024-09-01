0fe7a5b319fd6d0a1a1e9175761bfa68a03fcb03
HaroldR23 <harold.rodriguez@ioet.com>
2024-09-01 14:04:48 -0300

    refactor: follow 2 spaces indentantion pattern in constants folder

--- src/constants/productConstants.tsx
+++ src/constants/productConstants.tsx
@@ -1,9 +1,9 @@
 export const SELECT_OPTIONS = [
-    { label: "Superior e inferior", value: "superior_e_inferior" }, 
-    { label: "Valor individual", value: "valor_individual" },
-    { label: "Cada prótesis", value: "cada_protesis" },
-    { label: "De uno a tres dientes", value: "de_uno_a_tres_dientes" },
-    { label: "Cada diente", value: "cada_diente" },
+  { label: "Superior e inferior", value: "superior_e_inferior" }, 
+  { label: "Valor individual", value: "valor_individual" },
+  { label: "Cada prótesis", value: "cada_protesis" },
+  { label: "De uno a tres dientes", value: "de_uno_a_tres_dientes" },
+  { label: "Cada diente", value: "cada_diente" },
 ];
 
 export const API_URL = process.env.VITE_API_URL;
