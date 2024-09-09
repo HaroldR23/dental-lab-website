3fc9fec8528660bff86a6ead3b07ac2e1b922dc4
Harold Rodriguez <107003395+HaroldR23@users.noreply.github.com>
2024-09-08 21:47:18 -0300

    Hr/dlw add form validations (#41)
    * feat: add validation for product form creation
    
    * feat: clean up appointment form creation

--- src/components/molecules/CreateProductForm/CreateProductForm.tsx
+++ src/components/molecules/CreateProductForm/CreateProductForm.tsx
@@ -1,32 +1,39 @@
-import { Input, Select} from "antd"
+import { Input, Select } from "antd"
 import { CreateProductFormPropTypes } from "./CreateProductFormPropTypes";
 import { SELECT_OPTIONS } from "../../../constants/productConstants";
 
-const CreateProductForm = ({ onChange }: CreateProductFormPropTypes) => {
+const CreateProductForm = ({ onChange, errors }: CreateProductFormPropTypes) => {  // TODO: Implement add more than one price option
   return (
     <form className="createProductForm">
-      <Input 
-        placeholder="Nombre del producto" 
-        onChange={onChange}
-        name="name"
-        type="text"
-        style={{ margin: 8, width: "50%"}}
-      />
-      <Select 
-        placeholder="Seleccione una opción"
-        style={{ margin: 8, width: "50%"}}
-
-        options={SELECT_OPTIONS}
-        onChange={onChange}
-      />
-      <Input 
-        type="text" 
-        style={{ margin: 8, width: "50%"}}
-
-        placeholder="Precio" 
-        onChange={onChange}
-        name="price"
-      />
+      <div className="input">
+        <Input 
+          placeholder="Nombre del producto" 
+          onChange={onChange}
+          name="name"
+          type="text"
+          status={errors.name ? "error" : undefined}
+        />
+        {errors.name && <span className="error">{errors.name}</span>}
+      </div>
+      <div className="input">
+        <Select 
+          placeholder="Seleccione una opción"
+          status={errors.price ? "error" : undefined}
+          options={SELECT_OPTIONS}
+          onChange={onChange}
+        />
+        {errors.price && <span className="error">{errors.price}</span>}
+      </div>
+      <div className="input">
+        <Input 
+          type="text" 
+          status={errors.price ? "error" : undefined}
+          placeholder="Precio" 
+          onChange={onChange}
+          name="price"
+        />
+        {errors.price && <span className="error">{errors.price}</span>}
+      </div>
     </form>
   )
 };
