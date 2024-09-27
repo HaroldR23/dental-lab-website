2b2f0753ed312b219f25738fa1eba48fbdb546e6
HaroldR23 <harold.rodriguez@ioet.com>
2024-09-26 21:36:56 -0300

    refactor: make ButtonLink component responsive. Improve coverage

--- src/components/molecules/ButtonLink/ButtonLink.tsx
+++ src/components/molecules/ButtonLink/ButtonLink.tsx
@@ -1,12 +1,10 @@
 import { ButtonLinkPropTypes } from "./ButtonLinkPropTypes";
 
-const ButtonLink = ({ text, icon, disabled, ref_path, onClick }: ButtonLinkPropTypes) => {
+const ButtonLink = ({ text, icon, ref_path, onClick }: ButtonLinkPropTypes) => {
   return (
-    <a href={ref_path}>
-      <button className="buttonLink" disabled={disabled} onClick={onClick}>
-        {icon}
-        <div>{text}</div>
-      </button>
+    <a href={ref_path} className="buttonLink" onClick={onClick} >
+      {icon}
+      <div>{text}</div>
     </a>
   )
 }
--- src/components/molecules/ButtonLink/ButtonLinkPropTypes.ts
+++ src/components/molecules/ButtonLink/ButtonLinkPropTypes.ts
@@ -3,7 +3,6 @@ import { ReactElement } from "react"
 export interface ButtonLinkPropTypes {
   text: string
   icon: ReactElement
-  disabled?: boolean
   ref_path?: string
   onClick?: () => void
 }
