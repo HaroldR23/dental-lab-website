07768c7749518629a02f1d44afc1c085621a4339
HaroldR23 <harold.rodriguez@ioet.com>
2024-09-27 20:43:24 -0300

    refactor:  make Footer component responsive. Improve coverage

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,12 +1,14 @@
+import { useMediaQuery } from "react-responsive";
 import Copyright from "../../molecules/Copyright/Copyright";
 import FooterItems from "../FooterItems/FooterItems";
 import { FooterPropTypes } from "./FooterPropTypes";
 
 const Footer = ({ items, logoFooterUrl, logoCopyrightUrl, copyrightTextContent }: FooterPropTypes) => {
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 600px)' });
   return (
     <div id="footer">
       <div className="footer">
-        <img src={logoFooterUrl} alt='logotipo' width='135px' height='135px'/>
+        {!isSmallScreen && <img src={logoFooterUrl} alt='logotipo' width='135px' height='135px'/>}
         <FooterItems items={items}/>
       </div>
       <Copyright logoUrl={logoCopyrightUrl} textContent={copyrightTextContent}/>
