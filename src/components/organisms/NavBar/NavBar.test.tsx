76bb6e41d2f748858c3713b399af68128d08f349
Harold Rodriguez <107003395+HaroldR23@users.noreply.github.com>
2024-09-29 11:56:15 -0300

    refactor: make NavBar component responsive. Get better coverage (#45)

--- src/components/organisms/NavBar/NavBar.test.tsx
+++ src/components/organisms/NavBar/NavBar.test.tsx
@@ -1,7 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import NavBar from "./NavBar";
+import { useMediaQuery } from 'react-responsive';
+import { userEvent } from "@testing-library/user-event";
+
+
+jest.mock('react-responsive', () => ({
+  useMediaQuery: jest.fn(),
+}));
+
+const mockUseMediaQuery = useMediaQuery as jest.Mock;
 
 describe("NavBar", () => {
+  const user = userEvent.setup();
   const logoUrl = "https://example.com/logo.png";
   const anchorItems = [
     { title: "Home", href: "/", id: "home" },
@@ -11,7 +21,10 @@ describe("NavBar", () => {
   beforeEach(() => {
     render(<NavBar logoUrl={logoUrl} anchorItems={anchorItems} />);
   });
-  it("renders the logo", () => {        
+
+  it("should render the logo", () => {   
+    mockUseMediaQuery.mockReturnValue(false);
+  
     const logoElement = screen.queryByRole("img");
     const srcAttribute = logoElement?.getAttribute("src");
     const altAttribute = logoElement?.getAttribute("alt");
@@ -20,8 +33,26 @@ describe("NavBar", () => {
     expect(altAttribute).toBe(logoUrl);
   });
 
-  it("renders the anchor items", () => {
+  it("should render the anchor items", () => {
+    mockUseMediaQuery.mockReturnValue(false);
+
     const anchorElements = screen.queryByRole("list");
     expect(anchorElements?.childElementCount).toBe(anchorItems.length);
+  });
+
+  it("should render the menu icon", () => {
+    mockUseMediaQuery.mockReturnValue(true);
+    const menuIcon = screen.queryByRole("button");
+    expect(menuIcon).toBeDefined();
+  });
+
+  it("should render the anchor items in vertical container", async () => {
+    mockUseMediaQuery.mockReturnValue(true);
+
+    const button = screen.getByRole("button");
+    await user.click(button);
+    const verticalContainer = screen.queryByRole("list");
+  
+    expect(verticalContainer?.childElementCount).toBe(anchorItems.length);
   });
 });
